cf048b2abe6fb15133aecb7c3bb4c3498f4d990c
Viqui Alaro <viquialaro241@gmail.com>
2021-08-21 00:55:07 -0400

    inicio del proyecto de componentes

---
+++ lib/src/pages/home_temp.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+
+class HomePageTemp extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Home Page'),
+    );
+  }
+}
