f29034a5b21e93d4db5e4464441dbdd886ab43b4
Viqui Alaro <viquialaro241@gmail.com>
2021-08-24 23:28:11 -0400

    se realizo la implementación de la lectura de un json local

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:componentes/src/providers/menu_provider.dart';
 import 'package:flutter/material.dart';
 
 
@@ -16,6 +17,9 @@ class HomePage extends StatelessWidget {
   }
 
 Widget _lista() {
+
+  print(menuProvider.opciones);
+
   return ListView(
     children: _listaItems(),
   );
