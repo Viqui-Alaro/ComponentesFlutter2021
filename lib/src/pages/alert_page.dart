46b4d406ca02f360d90b65f09c477b6477aecb5d
Viqui Alaro <viquialaro241@gmail.com>
2021-08-29 23:06:30 -0400

    Implementación de las clases de alert y page se crearon los statelessWidget

---
+++ lib/src/pages/alert_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+ class AlertPage extends StatelessWidget {
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Alert Page'),
+      ),
+    );
+  }
+}
