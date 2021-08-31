3eb041423bce4b7f1ab20d2ce877f133e88bc16f
Viqui Alaro <viquialaro241@gmail.com>
2021-08-31 00:29:12 -0400

    implementacion de card Widget con los botones de cancelar y ok

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -28,6 +28,25 @@ Widget  _cardTipo1() {
       children: <Widget>[
         ListTile(
           leading: Icon(Icons.photo_album, color: Colors.blue),
+          title: Text('Hola Viqui Alaro Mamani'),
+          subtitle: Text('Bolivia es un país del centro de Sudamérica, con un terreno variado desde los Andes, el Desierto de Atacama y el bosque pluvial en la cuenca del Amazonas. A más de 3,500 m de altura, su capital administrativa, La Paz, se ubica en el Altiplano de los Andes con el monte nevado Illimani de fondo. Cerca se encuentra el cristalino lago Titicaca, el más grande del continente, que se extiende por la frontera con Perú'),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            TextButton(
+              child: Text('Cancelar'),
+              onPressed: (){
+
+              },
+            ),
+            TextButton(
+              child: Text('Ok'),
+              onPressed: (){
+                
+              },
+            ),
+          ],
         )
       ],
     ),
