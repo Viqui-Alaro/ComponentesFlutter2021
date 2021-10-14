2992d315be14087f008b87e0cd95fba1da9b298d
Viqui Alaro <viquialaro241@gmail.com>
2021-10-14 19:43:26 -0400

    implementacion de una alerta personalizada

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -12,12 +12,35 @@ import 'package:flutter/material.dart';
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed:()=> _mostrarAlert(context),
+          child: Text('Mostrar Alerta'),
+          style: ElevatedButton.styleFrom(
+            primary: Colors.red,
+            shape: StadiumBorder()
+          ),
+          )
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.backpack),
         onPressed: (){
           Navigator.pop(context);
         },
       ),
+    );
+  }
+
+  void _mostrarAlert(BuildContext context){
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context){
+        return AlertDialog(
+          title: Text('Titulo'),
+          content: Text('Contenido'),
+        );
+      }
     );
   }
 }
