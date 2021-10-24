27f0b1a3b720a4d30a8be0c5ed563c3b4c816ad1
Viqui Alaro <viquialaro241@gmail.com>
2021-10-24 18:14:44 -0400

    se implemento el avatar y se realizo repaso de container y fadeinimage

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -11,6 +11,30 @@ import 'package:flutter/material.dart';
     return Scaffold(
       appBar: AppBar(
         title: Text('Avater Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(4.0),
+            child: CircleAvatar(
+                backgroundImage: NetworkImage('https://www.buenamusica.com/media/fotos/cantantes/biografia/selena-gomez.jpg',scale: 1),
+                radius: 30.0,
+                
+            ),
+          ),
+          Container(
+            margin:EdgeInsets.only(right: 10.0),
+            child:CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.purple,
+            ) 
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+            image: NetworkImage('https://www.buenamusica.com/media/fotos/cantantes/biografia/selena-gomez.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'), 
+          fadeInDuration: Duration(milliseconds: 200),
+      )
       ),
     );
   }
