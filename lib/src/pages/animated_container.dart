86c7096ae8ac9af860acc71f85239554c6710aa2
Viqui Alaro <viquialaro241@gmail.com>
2021-10-24 22:57:25 -0400

    se realizo la implementacion de border radios

--- lib/src/pages/animated_container.dart
+++ lib/src/pages/animated_container.dart
@@ -2,13 +2,41 @@
 import 'package:flutter/material.dart';
 
 
-class sborrar extends StatelessWidget {
-  const sborrar({ Key? key }) : super(key: key);
+class AnimatedContainerPage extends StatefulWidget {
+  const AnimatedContainerPage({ Key? key }) : super(key: key);
+
+  @override
+  _AnimatedContainerPageState createState() => _AnimatedContainerPageState();
+}
+
+class _AnimatedContainerPageState extends State<AnimatedContainerPage> {
+
+  double _width  = 50.0;
+  double _height = 50.0;
+  Color _color   = Colors.pink;
+
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8.0);
+
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Animated Container'),
+
+      ),
+      body: Center(
+        child: Container(
+          width: _width,
+          height: _height,
+          decoration: BoxDecoration(
+            borderRadius: _borderRadius,
+            color: _color
+          ),
+
+        ),
+      ),
+
     );
   }
 }
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,10 +1,11 @@
+
 import 'package:flutter/material.dart';
 
 import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
-
+import 'package:componentes/src/pages/animated_container.dart';
 
 
 Map<String,WidgetBuilder> getApplicationRoutes(){
@@ -14,6 +15,7 @@ Map<String,WidgetBuilder> getApplicationRoutes(){
         'alert'  :  ( BuildContext context) => AlertPage(),
         'avatar' :  ( BuildContext context) => AvatarPage(),
         'card'   :  (BuildContext context) => CardPage(),
+        'animatedContainer'   :  (BuildContext context) => AnimatedContainerPage(),
       
  };
 
