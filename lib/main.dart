da85f06c3110e837d73e2de23a84b3118557b33d
Viqui Alaro <viquialaro241@gmail.com>
2021-10-24 22:18:02 -0400

    se realizo la implementacion de routes y asi como tambien se optimizo en el llamado y se realizo la implememntacion de avatar page

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 
-import 'package:componentes/src/pages/card_page.dart';
 import 'package:flutter/material.dart';
 
-
-import 'package:componentes/src/pages/home_page.dart';
 import 'package:componentes/src/pages/alert_page.dart';
-import 'package:componentes/src/pages/avatar_page.dart';
+import 'package:componentes/src/routes/routes.dart';
 
 void main() => runApp(MyApp());
  
@@ -17,12 +14,13 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       //home: HomePage(),
       initialRoute: '/',
-      routes:<String,WidgetBuilder>{
-        '/'      :  ( BuildContext context) => HomePage(),
-        'alert'  :  ( BuildContext context) => AlertPage(),
-        'avatar' :  ( BuildContext context) => AvatarPage(),
-        'card'   :  (BuildContext context) => CardPage(),
-      }
+      routes:getApplicationRoutes(),
+      onGenerateRoute:(RouteSettings settings){
+        
+        return MaterialPageRoute(
+          builder: (BuildContext context) => AlertPage()
+          );
+      } ,
     );
   }
 }
