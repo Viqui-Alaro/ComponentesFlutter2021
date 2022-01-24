dd6f3e87e03117bd9ae0b9ae2a88111f3f54e75d
Viqui Alaro <viquialaro241@gmail.com>
2022-01-24 18:44:45 -0400

    implementacion de template de homeScreem version Uno

---
+++ lib/src/pages/slider_page.dart
@@ -0,0 +1,17 @@
+import 'dart:html';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget{
+
+  const HomeScreen({Key? key}):super(key: key);
+  
+ @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('HomeScreen'),
+      ),
+    ) ;
+  }
+
+}
